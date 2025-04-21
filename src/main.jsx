8f2967d7a2731275b1b4085f2fb2a9add65ee44b
magis08 <agis7766@gmail.com>
2025-04-21 15:31:06 +0700

    move images to public folder

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import App from './App.jsx'
 
 import "@fontsource/outfit"
 import "@fontsource/roboto"
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
 
   </StrictMode>,
 )
