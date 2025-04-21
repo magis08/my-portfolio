342f7d4402d50600832ed990ccaebdd9e873cb8d
magis08 <agis7766@gmail.com>
2025-04-21 15:47:39 +0700

    edit utils

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -22,7 +22,7 @@ export const About = () => {
                 </div>
 
                 <img
-                    src={`${import.meta.env.BASE_URL}assets/about/about.png`}
+                    src={getImageUrl("about/about.png")}
                     alt="Developer at work"
                     className={styles.aboutImage}
                 />
